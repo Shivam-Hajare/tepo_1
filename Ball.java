4aa64885d5983eed13e8957314ddb84831476a4a
Karan Patil <karanp0202@gmail.com>
2021-07-21 23:30:55 +0530

    Update Ball.java

--- Ball.java
+++ Ball.java
@@ -1,73 +1,72 @@
 import java.awt.*;
 import java.awt.event.*;
 
-class Ball extends Frame implements Runnable,ActionListener
+class Ball extends Frame implements Runnable
 {
-	int x,y,dx,dy,rad,maxx,maxy;
+    int x,y,dx,dy,rad;
 
-	Ball()
-	{
-		x=y=50;
-		dx=dy=10;
-		rad=50;
-		maxx=400;
-		maxy=500;
+    static Ball obj=new Ball();
 
-		Thread t=new Thread(this);
-		t.start();
+    Ball()
+    {
+        x=y=50;
+        dx=dy=10;
+        rad=50;
 
-		this.setBackground(Color.yellow);
+        Thread t=new Thread(this);
+        t.start();
 
-		addWindowListener(new WindowAdapter()
-		{
-			public void windowClosing(WindowEvent we)
-			{
-				System.exit(0);
-			}
-		});
-		
-		
-	}
+        this.setBackground(Color.yellow);
 
-	public void run()
-	{
-		while(true)
-		{
-			try
-			{
-				Thread.sleep(100);
-			}
-			catch(InterruptedException ie)
-			{
-			}
-			x=x+dx;
-			y=y+dy;
+        addWindowListener(new WindowAdapter()
+        {
+            public void windowClosing(WindowEvent we)
+            {
+                System.exit(0);
+            }
+        });
 
-			if(x<=0 || (x+dx+rad)>=maxx)
-			{
-				dx=-dx;
-			}
-			if(y<=0 || (y+dy+rad)>=maxy)
-			{
-				dy=-dy;
-			}
-	
-			repaint();
-		}
-	}
 
-	public void paint(Graphics g)
-	{
-		//super.paint(g);
-		g.setColor(Color.red);
-		g.fillArc(x,y,rad,rad,0,360);
-	}
+    }
 
-	public static void main(String[]args)
-	{
-		Ball obj=new Ball();
-		obj.setTitle("Ball");
-		obj.setSize(400,500);
-		obj.setVisible(true);
-	}
+    public void run()
+    {
+        while(true)
+        {
+            try
+            {
+                Thread.sleep(16);
+            }
+            catch(InterruptedException ie)
+            {
+            }
+            x=x+dx;
+            y=y+dy;
+
+            if(x<=0 || (x+dx+rad)>=obj.getWidth())
+            {
+                dx=-dx;
+            }
+            if(y<=0 || (y+dy+rad)>=obj.getHeight())
+            {
+                dy=-dy;
+            }
+
+            repaint();
+        }
+    }
+
+    public void paint(Graphics g)
+    {
+        //super.paint(g);
+        g.setColor(Color.red);
+        g.fillArc(x,y,rad,rad,0,360);
+    }
+
+    public static void main(String[]args)
+    {
+        obj.setTitle("Ball");
+        obj.setSize(400,500);
+        obj.setVisible(true);
+    }
 }
